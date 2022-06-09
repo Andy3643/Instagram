2cbf200d59aec79960697b21f91242be78c81845
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-06-09 10:36:27 +0300

    fix:image upload

--- instagram/settings.py
+++ instagram/settings.py
@@ -42,7 +42,7 @@ STATICFILES_DIRS = (
 
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
 ALLOWED_HOSTS =config('ALLOWED_HOSTS', cast=Csv())
 
